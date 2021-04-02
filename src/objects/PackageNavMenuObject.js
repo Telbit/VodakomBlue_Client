4d2bd0c5def15666ae9b0776aa7c8dedc7d16610
Telbit <t.t.bende@gmail.com>
2021-04-02 07:49:31 +0200

    Add nav menu object

---
+++ src/objects/PackageNavMenuObject.js
@@ -0,0 +1,37 @@
+export const navMenu = {
+    home: {
+        name: "Home",
+        children: {
+            internet: {
+                name: "Internet",
+                formName: "homeInternet",
+                padding: 1,
+            },
+            tv: {
+                name: "Tv",
+                formName: "homeTv",
+                padding: 1
+            },
+            phone: {
+                name: "Phone",
+                formName: "homePhone",
+                padding: 1
+            }
+        }
+    },
+    mobile: {
+        name: "Mobile",
+        children: {
+            internet: {
+                name: "Internet",
+                formName: "mobileInternet",
+                padding: 1
+            },
+            phone: {
+                name: "Phone",
+                formName: "mobilePhone",
+                padding: 1
+            },
+        }
+    }
+};
