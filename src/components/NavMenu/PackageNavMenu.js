d0455891b863bfd49a425e3546716548f9d46c74
Telbit <t.t.bende@gmail.com>
2021-04-02 07:51:22 +0200

    Refactor PackageNavMenu to dynamic

--- src/components/NavMenu/PackageNavMenu.js
+++ src/components/NavMenu/PackageNavMenu.js
@@ -1,11 +1,9 @@
-import React, { useContext, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import Collapse from "@material-ui/core/Collapse";
-import { ExpandLess, ExpandMore } from "@material-ui/icons";
-import { FormContext } from "../../FormContext";
+import { Collapse, List, ListItem, ListItemText } from '@material-ui/core';
+import { ExpandLess, ExpandMore } from '@material-ui/icons';
+import React, { useContext, useState } from 'react'
+import { FormContext } from '../../FormContext';
+import { makeStyles } from '@material-ui/core';
+import { navMenu } from '../../objects/PackageNavMenuObject';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -18,45 +16,52 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
+const PrintNavButtons = (obj) => {
+    let listKey = 0;
+    const [IsOpen, setIsOpen] = useState(true);
+    const [, setFormName] = useContext(FormContext);
+    return (
+        Object.keys(obj).map((keyName) => {
+            if (keyName === "children") {
+                return (
+                    <div key={listKey++}>
+                        <ListItem style={{ paddingLeft: obj['padding'] * 50 }} button onClick={() => setIsOpen(!IsOpen)}>
+                            <ListItemText primary={obj["name"]}></ListItemText>{ }
+                            {IsOpen ? <ExpandLess /> : <ExpandMore />}
+                        </ListItem>
+                        <Collapse in={IsOpen} timeout="auto">
+                            <List disablePadding>
+                                {PrintNavButtons(obj[keyName])}
+                            </List>
+                        </Collapse>
+                    </div>
+                )
+            }
+            else if (typeof obj[keyName] === "object") {
+                return (
+                    PrintNavButtons(obj[keyName])
+                )
+            }
+
+            if (!obj["children"] && keyName === "name") {
+                return (
+                    <ListItem style={{ paddingLeft: obj['padding'] * 50 }} button key={listKey++} onClick={() => setFormName(obj["formName"])}>
+                        <ListItemText primary={obj[keyName]}></ListItemText>
+                    </ListItem>
+                )
+            }
+            else {
+                return null;
+            }
+        })
+    );
+};
+
 export const PackageNavMenu = () => {
     const classes = useStyles();
-    const [HomeOpen, setHomeOpen] = useState(true);
-    const [MobileOpen, setMobileOpen] = useState(true);
-    const [, setFormName] = useContext(FormContext);
-
     return (
-        <List className={classes.root}>
-            <ListItem button onClick={() => setHomeOpen(!HomeOpen)}>
-                <ListItemText primary="Home" />
-                {HomeOpen ? <ExpandLess /> : <ExpandMore />}
-            </ListItem>
-            <Collapse in={HomeOpen} timeout="auto" >
-                <List disablePadding>
-                    <ListItem button className={classes.nested} onClick={() => setFormName("homeInternet")}>
-                        <ListItemText primary="Internet" />
-                    </ListItem>
-                    <ListItem button className={classes.nested} onClick={() => setFormName("")}>
-                        <ListItemText primary="Tv" />
-                    </ListItem>
-                    <ListItem button className={classes.nested} onClick={() => setFormName("")}>
-                        <ListItemText primary="Phone" />
-                    </ListItem>
-                </List>
-            </Collapse>
-            <ListItem button onClick={() => setMobileOpen(!MobileOpen)}>
-                <ListItemText primary="Mobile" />
-                {MobileOpen ? <ExpandLess /> : <ExpandMore />}
-            </ListItem>
-            <Collapse in={MobileOpen} timeout="auto" >
-                <List disablePadding>
-                    <ListItem button className={classes.nested} onClick={() => setFormName("")}>
-                        <ListItemText primary="Internet" />
-                    </ListItem>
-                    <ListItem button className={classes.nested} onClick={() => setFormName("")}>
-                        <ListItemText primary="Phone" />
-                    </ListItem>
-                </List>
-            </Collapse>
+        <List className={classes.root} >
+            {PrintNavButtons(navMenu)}
         </List>
-    );
+    )
 }
