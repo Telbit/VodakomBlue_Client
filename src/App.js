38c80ceb3cbfa5bb81b5a8f72295b10de5bdb3db
kalacs <divixed12@gmail.com>
2021-04-13 17:37:24 +0200

    Add hr to route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Router, Route, Switch } from 'react-router-dom';
 import { Button, Grid, Typography } from '@material-ui/core';
 import Navbar from './components/Navbar/Navbar';
 import { EditLayout } from './components/Layout/EditLayout';
+import AddEmployee from './components/HRModule/Recruit/AddEmployee';
+import HRNavMenu from './components/NavMenu/HRNavMenu';
 
 export const App = () => {
     return (
@@ -14,6 +16,9 @@ export const App = () => {
                 </Route>
                 <Route path="/create">
                     <EditLayout />
+                </Route>
+                <Route path="/hr">
+                    <EditLayout navarea={<HRNavMenu/>} workarea={<AddEmployee/>}/>
                 </Route>
             </Switch>
         </div>
