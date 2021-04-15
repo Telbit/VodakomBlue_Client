e09a478a22f2764e5748154c909489333b278df6
Telbit <t.t.bende@gmail.com>
2021-04-15 09:52:52 +0200

    Add Home phone form

--- src/components/Layout/EditLayout.js
+++ src/components/Layout/EditLayout.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core';
 import { HomeInternetForm } from '../home/internet/HomeInternetForm';
 import { FormContext } from '../../FormContext'
 import { HomeTvForm } from '../home/Tv/HomeTvForm';
+import { HomePhoneForm } from '../home/phone/HomePhoneForm';
 
 const useStyles = makeStyles({
     container: {
@@ -23,6 +24,8 @@ const switchForm = (form) => {
             return <HomeInternetForm />
         case "homeTv":
             return <HomeTvForm />
+        case "homePhone":
+            return <HomePhoneForm />
         default:
             return <h1>Click to the home internet</h1>
     }
