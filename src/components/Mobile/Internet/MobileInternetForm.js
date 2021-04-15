f24f02886c6d61fbe7ef5a365718c12658aff7bc
Tomporino <tomorpeter97@gmail.com>
2021-04-15 13:59:40 +0100

    Set up base of MobilePackages components

---
+++ src/components/Mobile/Internet/MobileInternetForm.js
@@ -0,0 +1,74 @@
+import React, {useState, useEffect} from 'react';
+import { useSnackbar } from 'notistack';
+import {makeStyles, TextField} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    main: {
+
+    },
+}))
+
+export default function MobileInternetForm(){
+    const [Name, setName] = useState(null);
+    const [Price, setPrice] = useState(null);
+    const [ReleaseDate, setReleaseDate] = useState(null);
+    const [Size, setSize] = useState(null);
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+    const classes = useStyles();
+
+    let NameEvent = (event) => {
+        setName(event.target.value);
+    }
+
+    let PriceEvent = (event) => {
+        setPrice(event.target.value);
+    }
+
+    let ReleaseDateEvent = (event) => {
+        setReleaseDate(event.target.value);
+    }
+
+    let SizeEvent = (event) => {
+        setSize(event.target.value);
+    }
+
+    return(
+        <div className={classes.main}>
+            <form>
+                <TextField
+                    className={classes.input}
+                    label="Name"
+                    variant="outlined"
+                    required
+                    type="text"
+                    onChange={NameEvent}
+                />
+                <TextField
+                    className={classes.input}
+                    label="Price"
+                    variant="outlined"
+                    required
+                    type="number"
+                    onChange={PriceEvent}
+                />
+                <TextField
+                    className={classes.input}
+                    label="Date"
+                    variant="outlined"
+                    required
+                    type="date"
+                    onChange={ReleaseDateEvent}
+                />
+                <TextField
+                    className={classes.input}
+                    label="Size"
+                    variant="outlined"
+                    required
+                    type="number"
+                    onChange={SizeEvent}
+                />
+            </form>
+        </div>
+    )
+}
